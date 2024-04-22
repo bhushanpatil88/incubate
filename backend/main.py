c3175ae4e34c67dd7bb8ec046e22d10e61a1d894
Mehak-Chib <mehakchib26@gmail.com>
2024-04-23 01:23:36 +0530

    Adds graph.json

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import pandas as pd
 import csv
 from wmd import WMD
-from LLM import LLM
+from LLM import get_ideal_profiles
 import re
 
 app = Flask(__name__)
@@ -47,14 +47,8 @@ def main(idea, designation):
     if len(designations)==0:
         return jsonify({"error":"Error has occured"})
 
-    # LLM Prompter (Currently not working)
-    # llm = LLM()
-    # res = llm.get_ideal_profiles(idea=idea,designations=designations)
-    res = {
-        "ceo": "At the helm of the organization, an exceptional leader emerged, a visionary with an unparalleled ability to guide the company towards uncharted horizons. Possessing an insatiable curiosity and an unwavering belief in the power of innovation, this leader forged a path marked by strategic thinking and bold decisions. With a keen eye for emerging trends and a deep understanding of the industry landscape, they anticipated market shifts and identified growth opportunities that propelled the company to new heights. Their unwavering determination and risk-taking spirit inspired a culture of excellence within the organization, empowering employees to embrace challenges and strive for continuous improvement. By fostering collaboration and open communication, they created an environment where ideas thrived and transformative solutions were born. This visionary leader's exceptional decision-making skills, informed by rigorous data analysis and a deep comprehension of stakeholder perspectives, ensured that the company remained agile and responsive to market dynamics. Their unwavering belief in the power of people and their ability to drive change fostered a loyal and highly motivated workforce, dedicated to delivering exceptional results. Under their visionary leadership, the company scaled unprecedented heights, establishing itself as an industry leader and a beacon of innovation, leaving an indelible mark on the business world and beyond.",
-        "cmo": "",
-        "cto": "",
-    }
+    # LLM Prompter
+    res = get_ideal_profiles(idea=idea,designations=designations)
 
     # WMD Working
     final_results = {}
@@ -125,7 +119,6 @@ if __name__ == "__main__":
 
 #TODO
 """
-make LLM work
 use llm for generating synthetic names for profiles, for community and for people inside the community
 adjust the scores
 """
